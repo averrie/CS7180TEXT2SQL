035c1be1960a44703aa17f6a5dc2b996b6653876
Averrie <10875196+Averrie@users.noreply.github.com>
2025-04-06 02:30:32 -0700

    formatting and changing default model name to gemma 3 27b

--- methods/spider-agent-lite/spider_agent/agent/agents.py
+++ methods/spider-agent-lite/spider_agent/agent/agents.py
@@ -8,8 +8,26 @@ import uuid
 from http import HTTPStatus
 from io import BytesIO
 from typing import Dict, List
-from spider_agent.agent.prompts import BIGQUERY_SYSTEM, LOCAL_SYSTEM, DBT_SYSTEM, SNOWFLAKE_SYSTEM, REFERENCE_PLAN_SYSTEM
-from spider_agent.agent.action import Action, Bash, Terminate, CreateFile, EditFile, LOCAL_DB_SQL, BIGQUERY_EXEC_SQL, SNOWFLAKE_EXEC_SQL, BQ_GET_TABLES, BQ_GET_TABLE_INFO, BQ_SAMPLE_ROWS
+from spider_agent.agent.prompts import (
+    BIGQUERY_SYSTEM,
+    LOCAL_SYSTEM,
+    DBT_SYSTEM,
+    SNOWFLAKE_SYSTEM,
+    REFERENCE_PLAN_SYSTEM,
+)
+from spider_agent.agent.action import (
+    Action,
+    Bash,
+    Terminate,
+    CreateFile,
+    EditFile,
+    LOCAL_DB_SQL,
+    BIGQUERY_EXEC_SQL,
+    SNOWFLAKE_EXEC_SQL,
+    BQ_GET_TABLES,
+    BQ_GET_TABLE_INFO,
+    BQ_SAMPLE_ROWS,
+)
 from spider_agent.envs.spider_agent import Spider_Agent_Env
 from spider_agent.agent.models import call_llm
 
@@ -18,30 +36,28 @@ from openai import AzureOpenAI
 from typing import Dict, List, Optional, Tuple, Any, TypedDict
 
 
-
-
 logger = logging.getLogger("spider_agent")
 
 
 class PromptAgent:
     def __init__(
         self,
-        model="gpt-4",
+        model="gemma-3-27b-it",
         max_tokens=1500,
         top_p=0.9,
         temperature=0.5,
         max_memory_length=10,
         max_steps=15,
-        use_plan=False
+        use_plan=False,
     ):
-        
+
         self.model = model
         self.max_tokens = max_tokens
         self.top_p = top_p
         self.temperature = temperature
         self.max_memory_length = max_memory_length
         self.max_steps = max_steps
-        
+
         self.thoughts = []
         self.responses = []
         self.actions = []
@@ -52,7 +68,7 @@ class PromptAgent:
         self.codes = []
         self.work_dir = "/workspace"
         self.use_plan = use_plan
-        
+
     def set_env_and_task(self, env: Spider_Agent_Env):
         self.env = env
         self.thoughts = []
@@ -61,78 +77,149 @@ class PromptAgent:
         self.observations = []
         self.codes = []
         self.history_messages = []
-        self.instruction = self.env.task_config['question']
+        self.instruction = self.env.task_config["question"]
 
-        if self.env.task_config['type'] == 'Bigquery':
-            self._AVAILABLE_ACTION_CLASSES = [Bash, Terminate, BIGQUERY_EXEC_SQL, CreateFile, EditFile]
-            action_space = "".join([action_cls.get_action_description() for action_cls in self._AVAILABLE_ACTION_CLASSES])
-            self.system_message = BIGQUERY_SYSTEM.format(work_dir=self.work_dir, action_space=action_space, task=self.instruction, max_steps=self.max_steps)
-        elif self.env.task_config['type'] == 'Snowflake':
-            self._AVAILABLE_ACTION_CLASSES = [Bash, Terminate, SNOWFLAKE_EXEC_SQL, CreateFile, EditFile]
-            action_space = "".join([action_cls.get_action_description() for action_cls in self._AVAILABLE_ACTION_CLASSES])
-            self.system_message = SNOWFLAKE_SYSTEM.format(work_dir=self.work_dir, action_space=action_space, task=self.instruction, max_steps=self.max_steps)
-        elif self.env.task_config['type'] == 'Local':
-            self._AVAILABLE_ACTION_CLASSES = [Bash, Terminate, CreateFile, EditFile, LOCAL_DB_SQL]
-            action_space = "".join([action_cls.get_action_description() for action_cls in self._AVAILABLE_ACTION_CLASSES])
-            self.system_message = LOCAL_SYSTEM.format(work_dir=self.work_dir, action_space=action_space, task=self.instruction, max_steps=self.max_steps)
-        elif self.env.task_config['type'] == 'DBT':
-            self._AVAILABLE_ACTION_CLASSES = [Bash, Terminate, CreateFile, EditFile, LOCAL_DB_SQL]
-            action_space = "".join([action_cls.get_action_description() for action_cls in self._AVAILABLE_ACTION_CLASSES])
-            self.system_message = DBT_SYSTEM.format(work_dir=self.work_dir, action_space=action_space, task=self.instruction, max_steps=self.max_steps)
-        
-        if self.use_plan:
-            self.system_message += REFERENCE_PLAN_SYSTEM.format(plan=self.reference_plan)
-        
-
-        
-        self.history_messages.append({
-            "role": "system",
-            "content": [
-                {
-                    "type": "text",
-                    "text": self.system_message 
-                },
+        if self.env.task_config["type"] == "Bigquery":
+            self._AVAILABLE_ACTION_CLASSES = [
+                Bash,
+                Terminate,
+                BIGQUERY_EXEC_SQL,
+                CreateFile,
+                EditFile,
             ]
-        })
-        
-    def predict(self, obs: Dict=None) -> List:
+            action_space = "".join(
+                [
+                    action_cls.get_action_description()
+                    for action_cls in self._AVAILABLE_ACTION_CLASSES
+                ]
+            )
+            self.system_message = BIGQUERY_SYSTEM.format(
+                work_dir=self.work_dir,
+                action_space=action_space,
+                task=self.instruction,
+                max_steps=self.max_steps,
+            )
+        elif self.env.task_config["type"] == "Snowflake":
+            self._AVAILABLE_ACTION_CLASSES = [
+                Bash,
+                Terminate,
+                SNOWFLAKE_EXEC_SQL,
+                CreateFile,
+                EditFile,
+            ]
+            action_space = "".join(
+                [
+                    action_cls.get_action_description()
+                    for action_cls in self._AVAILABLE_ACTION_CLASSES
+                ]
+            )
+            self.system_message = SNOWFLAKE_SYSTEM.format(
+                work_dir=self.work_dir,
+                action_space=action_space,
+                task=self.instruction,
+                max_steps=self.max_steps,
+            )
+        elif self.env.task_config["type"] == "Local":
+            self._AVAILABLE_ACTION_CLASSES = [
+                Bash,
+                Terminate,
+                CreateFile,
+                EditFile,
+                LOCAL_DB_SQL,
+            ]
+            action_space = "".join(
+                [
+                    action_cls.get_action_description()
+                    for action_cls in self._AVAILABLE_ACTION_CLASSES
+                ]
+            )
+            self.system_message = LOCAL_SYSTEM.format(
+                work_dir=self.work_dir,
+                action_space=action_space,
+                task=self.instruction,
+                max_steps=self.max_steps,
+            )
+        elif self.env.task_config["type"] == "DBT":
+            self._AVAILABLE_ACTION_CLASSES = [
+                Bash,
+                Terminate,
+                CreateFile,
+                EditFile,
+                LOCAL_DB_SQL,
+            ]
+            action_space = "".join(
+                [
+                    action_cls.get_action_description()
+                    for action_cls in self._AVAILABLE_ACTION_CLASSES
+                ]
+            )
+            self.system_message = DBT_SYSTEM.format(
+                work_dir=self.work_dir,
+                action_space=action_space,
+                task=self.instruction,
+                max_steps=self.max_steps,
+            )
+
+        if self.use_plan:
+            self.system_message += REFERENCE_PLAN_SYSTEM.format(
+                plan=self.reference_plan
+            )
+
+        self.history_messages.append(
+            {
+                "role": "system",
+                "content": [
+                    {"type": "text", "text": self.system_message},
+                ],
+            }
+        )
+
+    def predict(self, obs: Dict = None) -> List:
         """
         Predict the next action(s) based on the current observation.
-        """    
-        
-        assert len(self.observations) == len(self.actions) and len(self.actions) == len(self.thoughts) \
-            , "The number of observations and actions should be the same."
+        """
+
+        assert len(self.observations) == len(self.actions) and len(self.actions) == len(
+            self.thoughts
+        ), "The number of observations and actions should be the same."
 
         status = False
         while not status:
             messages = self.history_messages.copy()
-            messages.append({
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Observation: {}\n".format(str(obs))
-                    }
-                ]
-            })  
-            status, response = call_llm({
-                "model": self.model,
-                "messages": messages,
-                "max_tokens": self.max_tokens,
-                "top_p": self.top_p,
-                "temperature": self.temperature
-            })
+            messages.append(
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "Observation: {}\n".format(str(obs))}
+                    ],
+                }
+            )
+            status, response = call_llm(
+                {
+                    "model": self.model,
+                    "messages": messages,
+                    "max_tokens": self.max_tokens,
+                    "top_p": self.top_p,
+                    "temperature": self.temperature,
+                }
+            )
             response = response.strip()
             if not status:
-                if response in ["context_length_exceeded","rate_limit_exceeded","max_tokens","unknown_error"]:
-                    self.history_messages = [self.history_messages[0]] + self.history_messages[3:]
+                if response in [
+                    "context_length_exceeded",
+                    "rate_limit_exceeded",
+                    "max_tokens",
+                    "unknown_error",
+                ]:
+                    self.history_messages = [
+                        self.history_messages[0]
+                    ] + self.history_messages[3:]
                 else:
                     raise Exception(f"Failed to call LLM, response: {response}")
-            
 
         try:
             action = self.parse_action(response)
-            thought = re.search(r'Thought:(.*?)Action', response, flags=re.DOTALL)
+            thought = re.search(r"Thought:(.*?)Action", response, flags=re.DOTALL)
             if thought:
                 thought = thought.group(1).strip()
             else:
@@ -140,7 +227,7 @@ class PromptAgent:
         except ValueError as e:
             print("Failed to parse action from response", e)
             action = None
-        
+
         logger.info("Observation: %s", obs)
         logger.info("Response: %s", response)
 
@@ -156,36 +243,45 @@ class PromptAgent:
         #     self.codes.append(None)
 
         return response, action
-        
-    
+
     def _add_message(self, observations: str, thought: str, action: Action):
-        self.history_messages.append({
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "Observation: {}".format(observations)
-                }
+        self.history_messages.append(
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Observation: {}".format(observations)}
+                ],
+            }
+        )
+        self.history_messages.append(
+            {
+                "role": "assistant",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Thought: {}\n\nAction: {}".format(
+                            thought, str(action)
+                        ),
+                    }
+                ],
+            }
+        )
+        if len(self.history_messages) > self.max_memory_length * 2 + 1:
+            self.history_messages = [self.history_messages[0]] + self.history_messages[
+                -self.max_memory_length * 2 :
             ]
-        })
-        self.history_messages.append({
-            "role": "assistant",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "Thought: {}\n\nAction: {}".format(thought, str(action))
-                }
-            ]
-        })
-        if len(self.history_messages) > self.max_memory_length*2+1:
-            self.history_messages = [self.history_messages[0]] + self.history_messages[-self.max_memory_length*2:]
-    
+
     def parse_action(self, output: str) -> Action:
-        """ Parse action from text """
+        """Parse action from text"""
         if output is None or len(output) == 0:
             pass
         action_string = ""
-        patterns = [r'["\']?Action["\']?:? (.*?)Observation',r'["\']?Action["\']?:? (.*?)Thought', r'["\']?Action["\']?:? (.*?)$', r'^(.*?)Observation']
+        patterns = [
+            r'["\']?Action["\']?:? (.*?)Observation',
+            r'["\']?Action["\']?:? (.*?)Thought',
+            r'["\']?Action["\']?:? (.*?)$',
+            r"^(.*?)Observation",
+        ]
 
         for p in patterns:
             match = re.search(p, output, flags=re.DOTALL)
@@ -194,7 +290,7 @@ class PromptAgent:
                 break
         if action_string == "":
             action_string = output.strip()
-        
+
         output_action = None
         for action_cls in self._AVAILABLE_ACTION_CLASSES:
             action = action_cls.parse_action_from_text(action_string)
@@ -202,17 +298,15 @@ class PromptAgent:
                 output_action = action
                 break
         if output_action is None:
-            action_string = action_string.replace("\_", "_").replace("'''","```")
+            action_string = action_string.replace("\_", "_").replace("'''", "```")
             for action_cls in self._AVAILABLE_ACTION_CLASSES:
                 action = action_cls.parse_action_from_text(action_string)
                 if action is not None:
                     output_action = action
                     break
-        
-        return output_action
-    
 
-    
+        return output_action
+
     def run(self):
         assert self.env is not None, "Environment is not set."
         result = ""
@@ -224,9 +318,7 @@ class PromptAgent:
         repeat_action = False
         while not done and step_idx < self.max_steps:
 
-            _, action = self.predict(
-                obs
-            )
+            _, action = self.predict(obs)
             if action is None:
                 logger.info("Failed to parse action from response, try again.")
                 retry_count += 1
@@ -259,17 +351,19 @@ class PromptAgent:
     def get_trajectory(self):
         trajectory = []
         for i in range(len(self.observations)):
-            trajectory.append({
-                "observation": self.observations[i],
-                "thought": self.thoughts[i],
-                "action": str(self.actions[i]),
-                # "code": self.codes[i],
-                "response": self.responses[i]
-            })
+            trajectory.append(
+                {
+                    "observation": self.observations[i],
+                    "thought": self.thoughts[i],
+                    "action": str(self.actions[i]),
+                    # "code": self.codes[i],
+                    "response": self.responses[i],
+                }
+            )
         trajectory_log = {
             "Task": self.instruction,
             "system_message": self.system_message,
-            "trajectory": trajectory
+            "trajectory": trajectory,
         }
         return trajectory_log
 
@@ -310,7 +404,6 @@ FROM bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*
 WHERE event_name = 'purchase' AND _TABLE_SUFFIX BETWEEN '20201201' AND '20201231'
 \"\"\", is_save=False)
 """
-
 
     action = agent.parse_action(response)
     print(action)
